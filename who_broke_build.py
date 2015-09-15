587495f3ff2e522ca9c4d573c1734a1298435f8e
zkan <kan@prontomarketing.com>
2015-09-15 08:16:22 +0700

    Introduce wait for event that always returns True

--- who_broke_build.py
+++ who_broke_build.py
@@ -7,6 +7,10 @@ from settings import (
 )
 
 
+def wait_for_event():
+    return True
+
+
 def jenkins_wait_for_event():
     sock = socket.socket(AF_INET, SOCK_DGRAM)
     sock.bind(('', JENKINS_NOTIFICATION_UDP_PORT))
--- who_broke_build_test.py
+++ who_broke_build_test.py
@@ -2,7 +2,10 @@ from mock import patch
 import socket
 import unittest
 
-from who_broke_build import jenkins_wait_for_event
+from who_broke_build import (
+    wait_for_event,
+    jenkins_wait_for_event
+)
 from settings import (
     AF_INET,
     SOCK_DGRAM,
@@ -25,6 +28,9 @@ class WhoBrokeBuildTest(unittest.TestCase):
         mock.socket.return_value.bind.assert_called_once_with(
             ('', JENKINS_NOTIFICATION_UDP_PORT)
         )
+
+    def test_wait_for_event_should_just_return_true(self):
+        self.assertTrue(wait_for_event())
 
 
 if __name__ == '__main__':
